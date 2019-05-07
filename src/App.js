463c77e2e3b8b969ebe07b2bd5b23526f299fb9b
howardbdev <howarddevennish@gmail.com>
2019-05-07 18:24:08 -0400

    rearrange state up to App and add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 import Cats from './components/Cats'
+import DisplayCat from './components/DisplayCat'
+import cats from './cats-data'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      cats: []
+    }
+  }
+
+  // mocking a fetch request
+  componentDidMount() {
+    // fetch("www.mycats.com")
+    //   .then(r=>r.json())
+    //   .then(cats => this.setState({cats: cats}))
+    this.setState({
+      cats: cats
+    })
+  }
+
+
   render() {
+
     return (
       <div className="App">
-        <Cats/>
+        { this.state.cats.length == 0
+            ? "LOADING..."
+            : <Fragment>
+                <div className="LikedCats">Liked</div>
+                <DisplayCat cat={this.state.cats[0]}/>
+                <div className="DislikedCats">Disliked</div>
+            </Fragment>
+      }
+        <Cats cats={this.state.cats}/>
       </div>
     );
   }
