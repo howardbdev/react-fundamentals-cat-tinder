aad6842a61c12abfe937f0f80644785a6a8e6a9f
howardbdev <howarddevennish@gmail.com>
2019-05-07 18:24:41 -0400

    update CatCard

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CatCard = ({ cat }) => {
+const CatCard = function({ cat }){
   return (
-    <div>
+    <div className="CatCard">
       <img height="100px" width="100px" src={cat.image_url} alt={cat.name}/>
       <p>{cat.name}</p>
     </div>
