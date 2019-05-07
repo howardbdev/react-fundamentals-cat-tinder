033a1a76943e7cc8dcbb526c195c449c9de548e0
howardbdev <howarddevennish@gmail.com>
2019-05-07 18:24:56 -0400

    update cats to functional component

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CatCard from './CatCard'
-import cats from '../cats-data'
 
-class Cats extends Component {
-  constructor(){
-    super()
-    this.state = {
-      cats: []
-    }
-  }
 
-  // mocking a fetch request
-  componentDidMount() {
-    // fetch("www.mycats.com")
-    //   .then(r=>r.json())
-    //   .then(cats => this.setState({cats: cats}))
-    this.setState({
-      cats: cats
-    })
-  }
-
-  render(){
-
-    const catCards = this.state.cats.map(cat => <CatCard key={cat.name} cat={cat}/>)
+const Cats = ({ cats }) => {
+  const catCards = cats.map(cat => <CatCard key={cat.name} cat={cat}/>)
 
     return (
-      <div>
+      <div className="Cats">
         { catCards }
       </div>
     )
-  }
 }
 
 export default Cats
