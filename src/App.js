fdd18029adcaf6944e6b267d190dbc384081aea9
howardbdev <howarddevennish@gmail.com>
2019-05-20 17:29:01 -0400

    move cats to redux

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Cats from './components/Cats'
 import CenterContainer from './components/CenterContainer'
+import { connect } from 'react-redux'
+import { getCats } from './actions/cats'
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      cats: []
-    }
-  }
 
   componentDidMount() {
-    fetch("http://localhost:3001/cats")
-      .then(r=>r.json())
-      .then(cats => this.setState({cats: cats}))
+    // fire off the getCats action
+    this.props.getCats()
   }
 
+  // turn this into an action/eliminate as a method of App
   handleLikeClick = (event) => {
     let newStatus
     if (event.target.tagName === "IMG") {newStatus = "undecided"}
     else {newStatus =  event.target.className === "like-button" ? "liked" : "disliked"}
-    const cats = this.state.cats.map(cat => {
+    const cats = this.props.cats.map(cat => {
       if (parseInt(cat.id) === parseInt(event.target.id) || cat.id == event.target.className) {
         const newCat = Object.assign({}, cat)
         newCat.status = newStatus
@@ -40,20 +36,34 @@ class App extends Component {
     return (
       <div className="App">
         <Cats
+          disliked={false}
           handleChangeOfHeart={this.handleLikeClick}
-          cats={this.state.cats.filter(cat => cat.status === "liked")}
+          cats={this.props.cats.filter(cat => cat.status === "liked")}
         />
         <CenterContainer
           handleLikeClick={this.handleLikeClick}
-          cats={this.state.cats.filter(cat => cat.status === "undecided")}/>
+        />
         <Cats
           disliked={true}
           handleChangeOfHeart={this.handleLikeClick}
-          cats={this.state.cats.filter(cat => cat.status === "disliked")}
+          cats={this.props.cats.filter(cat => cat.status === "disliked")}
         />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return ({
+    cats: state.cats
+  })
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return ({
+//     getCats: ()=>dispatch(getCats())
+//   })
+// }
+
+export default connect(mapStateToProps, { getCats })(App);
+//export default App
