3e2628186d2a9b4be5a7a9708a09c35e4fcb8f11
howardbdev <howarddevennish@gmail.com>
2019-05-20 17:28:37 -0400

    add redux - store - reducers, middleware, dev tools

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  combineReducers
+} from 'redux'
+import thunk from 'redux-thunk'
+import cats from './reducers/cats'
+// Redux libraries are redux, react-redux, redux-thunk
+// ingredients are reducers, store, actions (creators), ...???
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = combineReducers(
+  {
+    cats
+  }
+)
+
+const store = createStore(
+  reducers,
+  composeEnhancer(applyMiddleware(thunk)),
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
