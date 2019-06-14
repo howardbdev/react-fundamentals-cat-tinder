e99d2d557c154172ca1d3e1d796023de6b65477a
howardbdev <howarddevennish@gmail.com>
2019-06-14 10:59:49 -0400

    set up initial version of the store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  combineReducers
+} from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// I will import what I need to build a store from redux and react-redux
+// I will build the store
+  // I need at least one reducer
+  // ....?
+// I will wrap App in Provider, passing store as a prop to provider
+
+// build my reducer ------------------
+const catsReducer = (state=[], action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+const reducers = combineReducers({
+  cats: catsReducer
+})
+// ----------------------------------------
+
+// build my middleware ----------------------------
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//----------------------------------------------------
+
+
+// build my store -----------------------------------
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
+//----------------------------------------------------
+
+ReactDOM.render(<Provider store={store}>
+                  <App />
+                </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
